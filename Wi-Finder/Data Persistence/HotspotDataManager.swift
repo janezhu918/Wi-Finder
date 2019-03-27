98d3e1cb1602a6032d5cbd7d318b2365c8a76fc8
onielrosario <onielrosario@pursuit.org>
2019-03-27 15:28:58 -0400

    data persistence functions added

--- Wi-Finder/Data Persistence/HotspotDataManager.swift
+++ Wi-Finder/Data Persistence/HotspotDataManager.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+
+final class HotspotDataManager {
+    private init() {}
+   private static let hotspotFilename = "Hotspot.plist"
+    private static var hotspots = [Hotspot]()
+    
+    static func saveHotspot() {
+        let path = DataPersistenceManager.filepathToDocumentsDirectory(filename: hotspotFilename)
+        do {
+            let data = try PropertyListEncoder().encode(hotspots)
+            try data.write(to: path, options: Data.WritingOptions.atomic)
+        } catch {
+         print(error.localizedDescription)
+        }
+     
+    }
+    
+    static func getHotspots() -> [Hotspot] {
+        let path = DataPersistenceManager.filepathToDocumentsDirectory(filename: hotspotFilename).path
+        if FileManager.default.fileExists(atPath: path) {
+            if let data = FileManager.default.contents(atPath: path) {
+                do {
+                 hotspots = try PropertyListDecoder().decode([Hotspot].self, from: data)
+                } catch {
+                    print("property list decoding error: \(error)")
+                }
+            } else {
+                print("hotspots data is nil")
+            }
+        } else  {
+            print("\(hotspotFilename) does not exist.")
+        }
+        //sort hotspots
+        return hotspots
+    }
+    
+    
+    static func addHotspot(hotspot: Hotspot) {
+        hotspots.append(hotspot)
+        saveHotspot()
+    }
+    
+    static func deleteHotspot(atIndex: Int) {
+        hotspots.remove(at: atIndex)
+       saveHotspot()
+    }
+    
+    
+    
+    
+}
