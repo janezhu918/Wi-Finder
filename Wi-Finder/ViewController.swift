b1b5812588272fefdb1fdb41e15d22fded11f1d1
janezhu1618 <janezhu@pursuit.org>
2019-03-27 11:05:07 -0400

    first commit - setting up xcode file

---
+++ Wi-Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wi-Finder
+//
+//  Created by Jane Zhu on 3/27/19.
+//  Copyright © 2019 JaneZhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
