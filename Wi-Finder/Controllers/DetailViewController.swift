31c7be2f0a6e19d3606ee5dee48494b97edd362a
janezhu1618 <janezhu@pursuit.org>
2019-03-28 10:42:06 -0400

    mapview in detail vc working

--- Wi-Finder/Controllers/DetailViewController.swift
+++ Wi-Finder/Controllers/DetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailViewController: UIViewController {
     
@@ -17,8 +18,21 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(detailView)
-        detailView.infoTextView.text = "Address:\n\(hotspot.address)\n \(hotspot.city), NY \(hotspot.zipcode)\n\nSSID:\n\(hotspot.ssid)\n\nRemarks:\n\(hotspot.remarks)"
+        detailView.infoTextView.text = "Name:\n\(hotspot.locationName)\n\nAddress:\n\(hotspot.address)\n \(hotspot.city), NY \(hotspot.zipcode)\n\nSSID:\n\(hotspot.ssid)\n\nRemarks:\n\(hotspot.remarks)"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "•••", style: .plain, target: self, action: #selector(showActionSheet))
+        setupMap()
+    }
+    
+    private func setupMap() {
+        detailView.mapKitView.delegate = self
+        let regionRadius: CLLocationDistance = 650
+        let initialLocation = CLLocation(latitude: Double(hotspot!.lat) ?? 0.0, longitude: Double(hotspot!.long) ?? 0.0)
+        let coordinateRegion = MKCoordinateRegion(center: initialLocation.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        detailView.mapKitView.setRegion(coordinateRegion, animated: false)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: initialLocation.coordinate.latitude, longitude: initialLocation.coordinate.longitude)
+        annotation.title = hotspot.locationName
+        detailView.mapKitView.addAnnotation(annotation)
     }
     
     @objc private func showActionSheet() {
@@ -55,4 +69,13 @@ class DetailViewController: UIViewController {
         }
     }
     
+}
+
+extension DetailViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+        annotationView.markerTintColor = UIColor.init(displayP3Red: 247/255, green: 195/255, blue: 106/255, alpha: 1)
+        annotationView.glyphImage =  UIImage(named: "wifi")
+        return annotationView
+    }
 }
